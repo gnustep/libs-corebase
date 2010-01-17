e4c48e09f63fe0d61e2c280cf356368957484ae3
Stefan Bidigaray <stefanbidi@gmail.com>
2010-01-17 02:07:42 +0000

    Added CoreFoundation.h and fixed a typo in GNUmakefile
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/corebase/trunk@29301 72102866-910b-0410-8b05-ffd578937521

---
+++ Headers/CoreFoundation/CoreFoundation.h
@@ -0,0 +1,38 @@
+/* CoreFoundation.h
+   
+   Copyright (C) 2010 Free Software Foundation, Inc.
+   
+   Written by: Stefan Bidigaray
+   Date: January, 2010
+   
+   This file is part of CoreBase.
+   
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; see the file COPYING.LIB.
+   If not, see <http://www.gnu.org/licenses/> or write to the 
+   Free Software Foundation, 51 Franklin Street, Fifth Floor, 
+   Boston, MA 02110-1301, USA.
+*/ 
+
+#ifndef __COREFOUNDATION_COREFOUNDATION_H__
+#define __COREFOUNDATION_COREFOUNDATION_H__
+
+#include <CoreFoundation/CFArray.h>
+#include <CoreFoundation/CFBase.h>
+#include <CoreFoundation/CFCharacterSet.h>
+#include <CoreFoundation/CFData.h>
+#include <CoreFoundation/CFDictionary.h>
+#include <CoreFoundation/CFString.h>
+#include <CoreFoundation/CFStringEncodingExt.h>
+
+#endif /* __COREFOUNDATION_COREFOUNDATION_H__ */
