6124650f900b6e8a5153a5d9d11b4192d70df8db
Stefan Bidigaray <stefanbidi@gmail.com>
2010-01-20 03:23:50 +0000

    Fixed mistake in previous commit and added copyright notice.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/corebase/trunk@29320 72102866-910b-0410-8b05-ffd578937521

--- Headers/CoreFoundation/CFError.h
+++ Headers/CoreFoundation/CFError.h
@@ -1,3 +1,29 @@
+/* CFError.h
+   
+   Copyright (C) 2010 Free Software Foundation, Inc.
+   
+   Written by: Stefan Bidigaray
+   Date: January, 2010
+   
+   This file is part of GNUstep CoreBase Library.
+   
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; see the file COPYING.LIB.
+   If not, see <http://www.gnu.org/licenses/> or write to the 
+   Free Software Foundation, 51 Franklin Street, Fifth Floor, 
+   Boston, MA 02110-1301, USA.
+*/
+
 #ifndef __COREFOUNDATION_CFERROR_H__
 #define __COREFOUNDATION_CFERROR_H__
 
@@ -6,7 +32,7 @@
 #include <GNUstepBase/GSVersionMacros.h>
 
 // CFError implementation didn't show up until OS X 10.5
-#if OS_API_VERSION(GS_API_MACOSX_10_5, GS_API_LATEST)
+#if OS_API_VERSION(MAC_OS_X_VERSION_10_5, GS_API_LATEST)
 
 typedef struct __CFError * CFErrorRef;
 
@@ -62,6 +88,6 @@ CFErrorCopyRecoverySuggestion (CFErrorRef err);
 CFTypeID
 CFErrorGetTypeID (void);
 
-#endif
+#endif /* MAC_OS_X_VERSION_10_5 */
 
 #endif /* __COREFOUNDATION_CFERROR_H__ */
--- Source/CFError.m
+++ Source/CFError.m
@@ -1,5 +1,31 @@
-#include <Foundation/NSString.h> // For NSConstantString
-#include <Foundation/NSError.h>
+/* CFError.m
+   
+   Copyright (C) 2010 Free Software Foundation, Inc.
+   
+   Written by: Stefan Bidigaray
+   Date: January, 2010
+   
+   This file is part of GNUstep CoreBase Library.
+   
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; see the file COPYING.LIB.
+   If not, see <http://www.gnu.org/licenses/> or write to the 
+   Free Software Foundation, 51 Franklin Street, Fifth Floor, 
+   Boston, MA 02110-1301, USA.
+*/
+
+#import <Foundation/NSString.h> // For NSConstantString
+#import <Foundation/NSError.h>
 
 #include "CoreFoundation/CFError.h"
 
@@ -81,4 +107,3 @@ CFTypeID CFErrorGetTypeID (void)
 {
   return (CFTypeID)[NSError class];
 }
-
