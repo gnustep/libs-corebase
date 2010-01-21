3cc672b576401181224d333355f03f908ec69ae2
Eric Wasylishen <ewasylishen@gmail.com>
2010-01-21 22:42:46 +0000

    corebase: Start implementing CFURL
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/corebase/trunk@29352 72102866-910b-0410-8b05-ffd578937521

---
+++ Headers/CoreFoundation/CFURL.h
@@ -0,0 +1,69 @@
+/* CFURL.h
+   
+   Copyright (C) 2010 Free Software Foundation, Inc.
+   
+   Written by: Eric Wasylishen  <ewasylishen@gmail.com>
+   Date: January, 2010
+   
+   This file is part of CoreBase.
+   
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; see the file COPYING.LIB.
+   If not, see <http://www.gnu.org/licenses/> or write to the 
+   Free Software Foundation, 51 Franklin Street, Fifth Floor, 
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef __COREFOUNDATION_CFURL_H__
+#define __COREFOUNDATION_CFURL_H__
+
+#include <CoreFoundation/CFBase.h>
+
+typedef struct CFURL *CFURLRef;
+
+//
+// Constants
+//
+
+typedef enum CFURLPathStyle {
+  kCFURLPOSIXPathStyle = 0,
+  kCFURLHFSPathStyle = 1,
+  kCFURLWindowsPathStyle = 2
+} CFURLPathStyle;
+
+//
+// Creating a CFURL Object
+//
+
+CFURLRef
+CFURLCreateWithFileSystemPath (CFAllocatorRef allocator,
+ CFStringRef fileSystemPath, CFURLPathStyle style, Boolean isDirectory);
+
+CFURLRef
+CFURLCreateWithString (CFAllocatorRef allocator, CFStringRef string,
+  CFURLRef baseURL);
+
+//
+// Examining a CFURL Object
+//
+
+CFStringRef
+CFURLCopyFileSystemPath (CFURLRef aURL, CFURLPathStyle style);
+
+//
+// Getting the CFURL Type ID
+//
+CFTypeID
+CFURLGetTypeID (void);
+
+#endif /* __COREFOUNDATION_CFURL_H__ */
