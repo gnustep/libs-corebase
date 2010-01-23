e6be2abdb503a287678adef8e529fa0069a074e7
Stefan Bidigaray <stefanbidi@gmail.com>
2010-01-23 19:25:59 +0000

    Do it right this time.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/corebase/trunk@29369 72102866-910b-0410-8b05-ffd578937521

---
+++ Headers/CoreFoundation/CFStream.h
@@ -0,0 +1,301 @@
+/* CFStream.h
+   
+   Copyright (C) 2010 Free Software Foundation, Inc.
+   
+   Written by: Stefan Bidigaray
+   Date: January, 2010
+   
+   This file is part of GNUstep CoreBase Library.
+   
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; see the file COPYING.LIB.
+   If not, see <http://www.gnu.org/licenses/> or write to the 
+   Free Software Foundation, 51 Franklin Street, Fifth Floor, 
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef __COREFOUNDATION_CFSTREAM_H__
+#define __COREFOUNDATION_CFSTREAM_H__
+
+#include <CoreFoundation/CFBase.h>
+#include <CoreFoundation/CFError.h>
+#include <CoreFoundation/CFURL.h>
+
+typedef struct CFWriteStream *CFWriteStreamRef;
+typedef struct CFReadStream *CFReadStreamRef;
+
+
+
+#if GS_API_VERSION (MAC_OS_X_VERSION_10_1, GS_API_LATEST)
+
+const CFStringRef kCFStreamPropertyDataWritten;
+const CFStringRef kCFStreamPropertySocketNativeHandle;
+const CFStringRef kCFStreamPropertySocketRemoteHostName;
+const CFStringRef kCFStreamPropertySocketRemotePortNumber;
+#if GS_API_VERSION(MAC_OS_X_VERSION_10_2, GS_API_LATEST)
+const CFStringRef kCFStreamPropertyAppendToFile;
+#endif
+#if GS_API_VERSION(MAC_OS_X_VERSION_10_3, GS_API_LATEST)
+const CFStringRef kCFStreamPropertyFileCurrentOffset;
+#endif
+
+typedef struct CFStreamClientContext CFStreamClientContext;
+struct CFStreamClientContext
+{
+  CFIndex version;
+  void *info;
+  CFAllocatorRetainCallBack retain;
+  CFAllocatorReleaseCallBack release;
+  CFAllocatorCopyDescriptionCallBack copyDescription;
+};
+
+typedef enum _CFStreamStatus CFStreamStatus;
+enum _CFStreamStatus
+{
+  kCFStreamStatusNotOpen = 0,
+  kCFStreamStatusOpening,
+  kCFStreamStatusOpen,
+  kCFStreamStatusReading,
+  kCFStreamStatusWriting,
+  kCFStreamStatusAtEnd,
+  kCFStreamStatusClosed,
+  kCFStreamStatusError
+};
+
+// Deprecated
+typedef enum _CFStreamErrorDomain CFStreamErrorDomain;
+enum _CFStreamErrorDomain
+{
+  kCFStreamErrorDomainCustom = -1,
+  kCFStreamErrorDomainPOSIX = 1,
+  kCFStreamErrorDomainMacOSStatus
+};
+
+typedef struct _CFStreamError CFStreamError;
+struct _CFStreamError
+{
+  CFStreamErrorDomain domain;
+  SInt32 error;
+};
+
+typedef enum _CFStreamEventType CFStreamEventType;
+enum _CFStreamEventType
+{
+  kCFStreamEventNone = 0,
+  kCFStreamEventOpenCompleted = 1,
+  kCFStreamEventHasBytesAvailable = 2,
+  kCFStreamEventCanAcceptBytes = 4,
+  kCFStreamEventErrorOccurred = 8,
+  kCFStreamEventEndEncountered = 16
+};
+
+typedef void (*CFWriteStreamClientCallBack) (CFWriteStreamRef stream,
+  CFStreamEventType eventType, void *clientCallBackInfo);
+
+typedef void (*CFReadStreamClientCallBack) (CFReadStreamRef stream,
+  CFStreamEventType eventType, void *clientCallBackInfo);
+
+
+
+//
+// NSStream functions
+//
+/* FIXME: need CFSocket suff...
+void
+CFStreamCreatePairWithSocket (CFAllocatorRef alloc, CFSocketNativeHandle sock,
+                              CFReadStreamRef *readStream,
+                              CFWriteStreamRef *writeStream);
+*/
+void
+CFStreamCreatePairWithSocketToHost (CFAllocatorRef alloc, CFStringRef host,
+                                    UInt32 port, CFReadStreamRef *readStream,
+                                    CFWriteStreamRef *writeStream);
+
+//
+// Creating a Write Stream
+//
+CFWriteStreamRef
+CFWriteStreamCreateWithAllocatedBuffers (CFAllocatorRef alloc,
+                                         CFAllocatorRef bufferAllocator);
+
+CFWriteStreamRef
+CFWriteStreamCreateWithBuffer (CFAllocatorRef alloc, UInt8 *buffer,
+                               CFIndex bufferCapacity);
+
+CFWriteStreamRef
+CFWriteStreamCreateWithFile (CFAllocatorRef alloc, CFURLRef fileURL);
+
+//
+// Opening and Closing a Write Stream
+//
+void
+CFWriteStreamClose (CFWriteStreamRef stream);
+
+Boolean
+CFWriteStreamOpen (CFWriteStreamRef stream);
+
+//
+// Writing to a Stream
+//
+CFIndex
+CFWriteStreamWrite (CFWriteStreamRef stream, const UInt8 *buffer,
+                    CFIndex bufferLength);
+
+//
+// Scheduling a Write Stream
+//
+void
+CFWriteStreamScheduleWithRunLoop (CFWriteStreamRef stream,
+                                  CFRunLoopRef runLoop,
+                                  CFStringRef runLoopMode);
+
+void
+CFWriteStreamUnscheduleFromRunLoop (CFWriteStreamRef stream,
+                                    CFRunLoopRef runLoop,
+                                    CFStringRef runLoopMode);
+
+//
+// Examining Write Stream Properties
+//
+Boolean
+CFWriteStreamCanAcceptBytes (CFWriteStreamRef stream);
+
+CFTypeRef
+CFWriteStreamCopyProperty (CFWriteStreamRef stream, CFStringRef propertyName);
+
+// Deprecated function
+CFStreamError
+CFWriteStreamGetError (CFWriteStreamRef stream);
+
+CFStreamStatus
+CFWriteStreamGetStatus (CFWriteStreamRef stream);
+
+//
+// Setting Write Stream Properties
+//
+Boolean
+CFWriteStreamSetClient (CFWriteStreamRef stream, CFOptionFlags streamEvents,
+                        CFWriteStreamClientCallBack clientCB,
+                        CFStreamClientContext *clientContext);
+
+//
+// Getting the CFWriteStream Type ID
+//
+CFTypeID
+CFWriteStreamGetTypeID (void);
+
+//
+// Creating a Read Stream
+//
+CFReadStreamRef
+CFReadStreamCreateWithBytesNoCopy (CFAllocatorRef alloc, const UInt8 *bytes,
+                                   CFIndex length, CFAllocatorRef bytesDeallocator);
+
+CFReadStreamRef
+CFReadStreamCreateWithFile (CFAllocatorRef alloc, CFURLRef fileURL);
+
+//
+// Opening and Closing a Read Stream
+//
+void
+CFReadStreamClose (CFReadStreamRef stream);
+
+Boolean
+CFReadStreamOpen (CFReadStreamRef stream);
+
+//
+// Reading from a Read Stream
+//
+CFIndex
+CFReadStreamRead (CFReadStreamRef stream, UInt8 *buffer, CFIndex bufferLength);
+
+//
+// Scheduling a Read Stream
+//
+void
+CFReadStreamScheduleWithRunLoop (CFReadStreamRef stream, CFRunLoopRef runLoop,
+                                 CFStringRef runLoopMode);
+
+void
+CFReadStreamUnscheduleFromRunLoop (CFReadStreamRef stream, CFRunLoopRef runLoop,
+                                   CFStringRef runLoopMode);
+
+//
+// Examining Stream Properties
+//
+CFTypeRef
+CFReadStreamCopyProperty (CFReadStreamRef stream, CFStringRef propertyName);
+
+const UInt8 *
+CFReadStreamGetBuffer (CFReadStreamRef stream, CFIndex maxBytesToRead,
+                       CFIndex *numBytesRead);
+
+// Deprecated function
+CFStreamError
+CFReadStreamGetError (CFReadStreamRef stream);
+
+
+CFStreamStatus
+CFReadStreamGetStatus (CFReadStreamRef stream);
+
+Boolean
+CFReadStreamHasBytesAvailable (CFReadStreamRef stream);
+
+//
+// Setting Stream Properties
+//
+Boolean
+CFReadStreamSetClient (CFReadStreamRef stream, CFOptionFlags streamEvents,
+                       CFReadStreamClientCallBack clientCB,
+                       CFStreamClientContext *clientContext);
+
+//
+// Getting the CFReadStream Type ID
+//
+CFTypeID
+CFReadStreamGetTypeID (void);
+
+
+
+#if GS_API_VERSION(MAC_OS_X_VERSION_10_2, GS_API_LATEST)
+/* FIXME: need CFSocket stuff
+void
+CFStreamCreatePairWithPeerSocketSignature (CFAllocatorRef alloc,
+                                           const CFSocketSignature *signature,
+                                           CFReadStreamRef *readStream,
+                                           CFWriteStreamRef *writeStream);
+*/
+Boolean
+CFWriteStreamSetProperty (CFWriteStreamRef stream, CFStringRef propertyName,
+                          CFTypeRef propertyValue);
+
+Boolean
+CFReadStreamSetProperty (CFReadStreamRef stream, CFStringRef propertyName,
+                         CFTypeRef propertyValue);
+#endif
+
+#if GS_API_VERSION(MAC_OS_X_VERSION_10_5, GS_API_LATEST)
+void
+CFStreamCreateBoundPair (CFAllocatorRef alloc, CFReadStreamRef *readStream,
+                         CFWriteStreamRef *writeStream, CFIndex transferBufferSize);
+
+CFErrorRef
+CFWriteStreamCopyError (CFWriteStreamRef stream);
+
+CFErrorRef
+CFReadStreamCopyError (CFReadStreamRef stream);
+#endif
+
+#endif /* MAC_OS_X_VERSION_10_1 */
+
+#endif /* __COREFOUNDATION_CFSTREAM_H__ */
---
+++ Source/CFStream.m
@@ -0,0 +1,356 @@
+/* CFStream.m
+   
+   Copyright (C) 2010 Free Software Foundation, Inc.
+   
+   Written by: Stefan Bidigaray
+   Date: January, 2010
+   
+   This file is part of GNUstep CoreBase Library.
+   
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; see the file COPYING.LIB.
+   If not, see <http://www.gnu.org/licenses/> or write to the 
+   Free Software Foundation, 51 Franklin Street, Fifth Floor, 
+   Boston, MA 02110-1301, USA.
+*/
+
+#import <Foundation/NSStream.h>
+#import <Foundation/NSHost.h>
+#import <Foundation/NSURL.h>
+
+#include "CoreFoundation/CFStream.h"
+
+void
+_CFStreamClose (CFTypeRef stream)
+{
+  [(NSStream *)stream close];
+}
+
+CFErrorRef
+_CFStreamCopyError (CFTypeRef stream)
+{
+  return CFRetain([(NSStream *)stream streamError]);
+}
+
+CFTypeRef
+_CFStreamCopyProperty (CFTypeRef stream, CFStringRef propertyName)
+{
+  return CFRetain([(NSStream *)stream propertyForKey: (NSString *)propertyName]);
+}
+
+Boolean
+_CFStreamSetProperty (CFTypeRef stream, CFStringRef propertyName,
+                      CFTypeRef propertyValue)
+{
+  return (Boolean)[(NSStream *)stream setProperty: (id)propertyValue
+                                           forKey: (NSString *)propertyName];
+}
+
+CFStreamError
+_CFStreamGetError (CFTypeRef stream)
+{
+  // FIXME
+  return (CFStreamError){0, 0};
+}
+
+CFStreamStatus
+_CFStreamGetStatus (CFTypeRef stream)
+{
+  return [(NSStream *)stream streamStatus];
+}
+
+Boolean
+_CFStreamOpen (CFTypeRef stream)
+{
+  Boolean ret;
+  
+  [(NSStream *)stream open];
+  return (_CFStreamGetStatus (stream) == kCFStreamStatusOpen ? true : false);
+}
+
+void
+_CFStreamScheduleWithRunLoop (CFTypeRef stream, CFRunLoopRef runLoop,
+                              CFStringRef runLoopMode)
+{
+  [(NSStream *)stream scheduleInRunLoop: (NSRunLoop *)runLoop
+                                forMode: (NSString *)runLoopMode];
+}
+
+void
+_CFStreamUncheduleWithRunLoop (CFTypeRef stream, CFRunLoopRef runLoop,
+                               CFStringRef runLoopMode)
+{
+  [(NSStream *)stream removeFromRunLoop: (NSRunLoop *)runLoop
+                                forMode: (NSString *)runLoopMode];
+}
+
+
+
+void
+CFStreamCreateBoundPair (CFAllocatorRef alloc, CFReadStreamRef *readStream,
+                         CFWriteStreamRef *writeStream, CFIndex transferBufferSize)
+{
+  // FIXME
+}
+
+/*void
+CFStreamCreatePairWithPeerSocketSignature (CFAllocatorRef alloc,
+                                           const CFSocketSignature *signature,
+                                           CFReadStreamRef *readStream,
+                                           CFWriteStreamRef *writeStream)
+{
+  // FIXME
+}
+
+void
+CFStreamCreatePairWithSocket (CFAllocatorRef alloc, CFSocketNativeHandle sock,
+                              CFReadStreamRef *readStream,
+                              CFWriteStreamRef *writeStream)
+{
+  // FIXME
+}
+*/
+void
+CFStreamCreatePairWithSocketToHost (CFAllocatorRef alloc, CFStringRef host,
+                                    UInt32 port, CFReadStreamRef *readStream,
+                                    CFWriteStreamRef *writeStream)
+{
+  [[NSStream allocWithZone: alloc]
+    getStreamsToHost: [NSHost hostWithName: (NSString *)host]
+                port: (NSUInteger)port
+         inputStream: (NSInputStream **)readStream
+        outputStream: (NSOutputStream **)writeStream];
+}
+
+
+
+Boolean
+CFWriteStreamCanAcceptBytes (CFWriteStreamRef stream)
+{
+  return (Boolean)[(NSOutputStream *)stream hasSpaceAvailable];
+}
+
+void
+CFWriteStreamClose (CFWriteStreamRef stream)
+{
+  _CFStreamClose (stream);
+}
+
+CFErrorRef
+CFWriteStreamCopyError (CFWriteStreamRef stream)
+{
+  return _CFStreamCopyError (stream);
+}
+
+CFTypeRef
+CFWriteStreamCopyProperty (CFWriteStreamRef stream, CFStringRef propertyName)
+{
+  return _CFStreamCopyProperty (stream, propertyName);
+}
+
+CFWriteStreamRef
+CFWriteStreamCreateWithAllocatedBuffers (CFAllocatorRef alloc,
+                                         CFAllocatorRef bufferAllocator)
+{
+  // FIXME: ???
+}
+
+CFWriteStreamRef
+CFWriteStreamCreateWithBuffer (CFAllocatorRef alloc, UInt8 *buffer,
+                               CFIndex bufferCapacity)
+{
+  return (CFWriteStreamRef)[[NSOutputStream allocWithZone: (NSZone *)alloc]
+                             initToBuffer: buffer
+                                 capacity: (NSUInteger)bufferCapacity];
+}
+
+CFWriteStreamRef
+CFWriteStreamCreateWithFile (CFAllocatorRef alloc, CFURLRef fileURL)
+{
+  // FIXME: there's nothing in -base to set the append option at a later time.
+  return (CFWriteStreamRef)[[NSOutputStream allocWithZone: (NSZone *)alloc]
+                             initToFileAtPath: [(NSURL *)fileURL absoluteString]
+                                       append: NO];
+}
+
+CFStreamError
+CFWriteStreamGetError (CFWriteStreamRef stream)
+{
+  return _CFStreamGetError (stream);
+}
+
+CFStreamStatus
+CFWriteStreamGetStatus (CFWriteStreamRef stream)
+{
+  return _CFStreamGetStatus (stream);
+}
+
+CFTypeID
+CFWriteStreamGetTypeID (void)
+{
+  return (CFTypeID)[NSOutputStream class];
+}
+
+Boolean
+CFWriteStreamOpen (CFWriteStreamRef stream)
+{
+  _CFStreamOpen (stream);
+}
+
+void
+CFWriteStreamScheduleWithRunLoop (CFWriteStreamRef stream,
+                                  CFRunLoopRef runLoop,
+                                  CFStringRef runLoopMode)
+{
+  _CFStreamScheduleWithRunLoop (stream, runLoop, runLoopMode);
+}
+
+Boolean
+CFWriteStreamSetClient (CFWriteStreamRef stream, CFOptionFlags streamEvents,
+                        CFWriteStreamClientCallBack clientCB,
+                        CFStreamClientContext *clientContext)
+{
+  // FIXME
+  return false;
+}
+
+Boolean
+CFWriteStreamSetProperty (CFWriteStreamRef stream, CFStringRef propertyName,
+                          CFTypeRef propertyValue)
+{
+  return _CFStreamSetProperty (stream, propertyName, propertyValue);
+}
+
+void
+CFWriteStreamUnscheduleFromRunLoop (CFWriteStreamRef stream,
+                                    CFRunLoopRef runLoop,
+                                    CFStringRef runLoopMode)
+{
+  _CFStreamUnscheduleFromRunLoop (stream, runLoop, runLoopMode);
+}
+
+CFIndex
+CFWriteStreamWrite (CFWriteStreamRef stream, const UInt8 *buffer,
+                    CFIndex bufferLength)
+{
+  return (CFIndex)[(NSOutputStream *)stream write: buffer
+                                        maxLength: (NSUInteger)bufferLength];
+}
+
+
+
+void
+CFReadStreamClose (CFReadStreamRef stream)
+{
+  [(NSInputStream *)stream close];
+}
+
+CFErrorRef
+CFReadStreamCopyError (CFReadStreamRef stream)
+{
+  return (CFErrorRef)[(NSInputStream *)stream streamError];
+}
+
+CFTypeRef
+CFReadStreamCopyProperty (CFReadStreamRef stream, CFStringRef propertyName)
+{
+  return _CFStreamCopyProperty (stream, propertyName);
+}
+
+CFReadStreamRef
+CFReadStreamCreateWithBytesNoCopy (CFAllocatorRef alloc, const UInt8 *bytes,
+                                   CFIndex length, CFAllocatorRef bytesDeallocator)
+{
+  // FIXME
+}
+
+CFReadStreamRef
+CFReadStreamCreateWithFile (CFAllocatorRef alloc, CFURLRef fileURL)
+{
+  return (CFReadStreamRef)[[NSInputStream allocWithZone: alloc]
+                           initWithFileAtPath: [(NSURL *)fileURL absoluteString]];
+}
+
+const UInt8 *
+CFReadStreamGetBuffer (CFReadStreamRef stream, CFIndex maxBytesToRead,
+                       CFIndex *numBytesRead)
+{
+  // FIXME: docs are  bit confusing
+}
+
+CFStreamError
+CFReadStreamGetError (CFReadStreamRef stream)
+{
+  return _CFStreamGetError (stream);
+}
+
+CFStreamStatus
+CFReadStreamGetStatus (CFReadStreamRef stream)
+{
+  return (CFStreamStatus)[(NSInputStream *)stream streamStatus];
+}
+
+CFTypeID
+CFReadStreamGetTypeID (void)
+{
+  return (CFTypeID)[NSInputStream class];
+}
+
+Boolean
+CFReadStreamHasBytesAvailable (CFReadStreamRef stream)
+{
+  return [(NSInputStream *)stream hasBytesAvailable];
+}
+
+Boolean
+CFReadStreamOpen (CFReadStreamRef stream)
+{
+  return _CFStreamOpen (stream);
+}
+
+CFIndex
+CFReadStreamRead (CFReadStreamRef stream, UInt8 *buffer, CFIndex bufferLength)
+{
+  return (CFIndex)[(NSInputStream *)stream read: buffer
+                                      maxLength: (NSUInteger)bufferLength];
+}
+
+void
+CFReadStreamScheduleWithRunLoop (CFReadStreamRef stream, CFRunLoopRef runLoop,
+                                 CFStringRef runLoopMode)
+{
+  _CFStreamSscheduleFromRunLoop (stream, runLoop, runLoopMode);
+}
+
+Boolean
+CFReadStreamSetClient (CFReadStreamRef stream, CFOptionFlags streamEvents,
+                       CFReadStreamClientCallBack clientCB,
+                       CFStreamClientContext *clientContext)
+{
+  // FIXME
+  return false;
+}
+
+Boolean
+CFReadStreamSetProperty (CFReadStreamRef stream, CFStringRef propertyName,
+                         CFTypeRef propertyValue)
+{
+  return _CFStreamSetProperty (stream, propertyName, propertyValue);
+}
+
+void
+CFReadStreamUnscheduleFromRunLoop (CFReadStreamRef stream, CFRunLoopRef runLoop,
+                                   CFStringRef runLoopMode)
+{
+  _CFStreamUnscheduleFromRunLoop (stream, runLoop, runLoopMode);
+}
