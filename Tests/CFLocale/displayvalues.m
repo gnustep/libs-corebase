fb3bf4c9834fc448404e015c02ce02ebf4bf18b8
Stefan Bidigaray <stefanbidi@gmail.com>
2011-03-26 21:14:24 +0000

    Added tests for CFLocaleCopyDisplayNameForPropertyValue().
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/corebase/trunk@32712 72102866-910b-0410-8b05-ffd578937521

---
+++ Tests/CFLocale/displayvalues.m
@@ -0,0 +1,49 @@
+#include "CoreFoundation/CFString.h"
+#include "CoreFoundation/CFLocale.h"
+#include "../CFTesting.h"
+
+int main (void)
+{
+  CFLocaleRef locale;
+  CFStringRef str;
+  
+  locale = CFLocaleCreate (NULL,
+    CFSTR("en_US"));
+  str = CFLocaleCopyDisplayNameForPropertyValue (locale,
+    kCFLocaleIdentifier,
+    CFSTR("pt_BR@calendar=gregorian;collation=traditional;currency=BRL"));
+  PASS_CFEQ(str, CFSTR("Portuguese (Brazil, calendar=Gregorian Calendar, collation=Traditional Sort Order, currency=Brazilian Real)"),
+    "Display identifier is correct");
+  str = CFLocaleCopyDisplayNameForPropertyValue (locale,
+    kCFLocaleLanguageCode,
+    CFSTR("pt_BR@calendar=gregorian;collation=traditional;currency=BRL"));
+  PASS_CFEQ(str, CFSTR("Portuguese"), "Display language is correct");
+  str = CFLocaleCopyDisplayNameForPropertyValue (locale,
+    kCFLocaleCountryCode,
+    CFSTR("pt_BR@calendar=gregorian;collation=traditional;currency=BRL"));
+  PASS_CFEQ(str, CFSTR("Brazil"), "Display country is correct");
+  str = CFLocaleCopyDisplayNameForPropertyValue (locale,
+    kCFLocaleScriptCode,
+    CFSTR("pt_BR@calendar=gregorian;collation=traditional;currency=BRL"));
+  PASS_CFEQ(str, NULL, "Display script is correct");
+  str = CFLocaleCopyDisplayNameForPropertyValue (locale,
+    kCFLocaleVariantCode,
+    CFSTR("pt_BR@calendar=gregorian;collation=traditional;currency=BRL"));
+  PASS_CFEQ(str, NULL, "Display variant is correct");
+  str = CFLocaleCopyDisplayNameForPropertyValue (locale,
+    kCFLocaleCalendarIdentifier,
+    CFSTR("pt_BR@calendar=gregorian;collation=traditional;currency=BRL"));
+  PASS_CFEQ(str, CFSTR("Gregorian Calendar"),
+    "Display calendar identifier is correct");
+  str = CFLocaleCopyDisplayNameForPropertyValue (locale,
+    kCFLocaleCollationIdentifier,
+    CFSTR("pt_BR@calendar=gregorian;collation=traditional;currency=BRL"));
+  PASS_CFEQ(str, CFSTR("Traditional Sort Order"),
+    "Display collation identifier is correct");
+  str = CFLocaleCopyDisplayNameForPropertyValue (locale,
+    kCFLocaleCurrencyCode,
+    CFSTR("pt_BR@calendar=gregorian;collation=traditional;currency=BRL"));
+  PASS_CFEQ(str, CFSTR("Brazilian Real"), "Display currency code is correct");
+  
+  return 0;
+}
