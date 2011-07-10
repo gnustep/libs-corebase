6429efe44d2b0617567495802abeb0dffd30056b
Stefan Bidigaray <stefanbidi@gmail.com>
2011-07-10 23:31:39 +0000

    Forgot to tests
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/corebase/trunk@33507 72102866-910b-0410-8b05-ffd578937521

--- Tests/CFCalendar/basic.m
+++ Tests/CFCalendar/basic.m
@@ -8,12 +8,19 @@ int main (void)
 {
   CFCalendarRef cal;
   CFAbsoluteTime at = 0.0;
+  int year, month, hour;
   
   cal = CFCalendarCreateWithIdentifier (NULL, kCFGregorianCalendar);
   PASS(CFCalendarComposeAbsoluteTime(cal, &at, "yMd", 2011, 7, 8),
     "Absolute time composed.");
-  printf ("%f", at);
-  PASS(at == 2291090400.0, "Absolute time composed correctly.");
+  PASS(at == 334476000.0, "Absolute time composed correctly.");
+  
+  PASS(CFCalendarDecomposeAbsoluteTime(cal, at, "yMh", &year, &month, &hour),
+    "Absolute time decomposed.");
+  PASS(year == 2011, "Year decomposed correctly.");
+  PASS(month == 7, "Month decomosed correctly.");
+  PASS(hour == 7, "Hour decomposed correctly.");
+  
   CFRelease (cal);
   
   return 0;
