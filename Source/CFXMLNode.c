21b33b22a73874a3c3a5bbc55042d1667aa06b8d
Stefan Bidigaray <stefanbidi@gmail.com>
2011-12-17 17:26:37 +0000

    Started adding code to CFXMLNode.  Still needs some more work.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/corebase/trunk@34313 72102866-910b-0410-8b05-ffd578937521

--- Source/CFXMLNode.c
+++ Source/CFXMLNode.c
@@ -25,9 +25,38 @@
 */
 
 #include "CoreFoundation/CFRuntime.h"
+#include "CoreFoundation/CFBase.h"
+#include "CoreFoundation/CFString.h"
 #include "CoreFoundation/CFXMLNode.h"
 
+struct __CFXMLNode
+{
+  CFRuntimeBase _parent;
+  CFStringRef   _string;
+  const void   *_info;
+  CFIndex       _version;
+};
+
 static CFTypeID _kCFXMLNodeTypeID = 0;
+
+static const CFRuntimeClass CFXMLNodeClass =
+{
+  0,
+  "CFXMLNode",
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL,
+  NULL
+};
+
+void CFXMLNodeInitialize (void)
+{
+  _kCFXMLNodeTypeID = _CFRuntimeRegisterClass (&CFXMLNodeClass);
+}
+
+
 
 CFTypeID
 CFXMLNodeGetTypeID (void)
@@ -35,11 +64,62 @@ CFXMLNodeGetTypeID (void)
   return _kCFXMLNodeTypeID;
 }
 
+#define CFXMLNODE_SIZE (sizeof(struct __CFXMLNode) - sizeof(CFRuntimeBase))
+
 CFXMLNodeRef
 CFXMLNodeCreate (CFAllocatorRef alloc, CFXMLNodeTypeCode xmlType,
   CFStringRef dataString, const void *additionalInfoPtr, CFIndex version)
 {
-  return NULL;
+  struct __CFXMLNode *new;
+  CFIndex additionalInfoSize;
+  
+  switch (xmlType)
+    {
+      case kCFXMLNodeTypeDocument:
+        additionalInfoSize = sizeof(struct CFXMLDocumentInfo);
+        break;
+      case kCFXMLNodeTypeElement:
+        additionalInfoSize = sizeof(struct CFXMLElementInfo);
+        break;
+      case kCFXMLNodeTypeProcessingInstruction:
+        additionalInfoSize = sizeof(struct CFXMLProcessingInstructionInfo);
+        break;
+      case kCFXMLNodeTypeEntity:
+        additionalInfoSize = sizeof(struct CFXMLEntityInfo);
+        break;
+      case kCFXMLNodeTypeEntityReference:
+        additionalInfoSize = sizeof(struct CFXMLEntityReferenceInfo);
+        break;
+      case kCFXMLNodeTypeDocumentType:
+        additionalInfoSize = sizeof(struct CFXMLDocumentTypeInfo);
+        break;
+      case kCFXMLNodeTypeNotation:
+        additionalInfoSize = sizeof(struct CFXMLNotationInfo);
+        break;
+      case kCFXMLNodeTypeElementTypeDeclaration:
+        additionalInfoSize = sizeof(struct CFXMLElementTypeDeclarationInfo);
+        break;
+      case kCFXMLNodeTypeAttributeListDeclaration:
+        additionalInfoSize = sizeof(struct CFXMLAttributeListDeclarationInfo);
+        break;
+      default:
+        additionalInfoSize = 0;
+    }
+  
+  new = (struct __CFXMLNode*)_CFRuntimeCreateInstance (alloc, _kCFXMLNodeTypeID,
+    CFXMLNODE_SIZE + additionalInfoSize, 0);
+  if (new)
+    {
+      new->_string = CFStringCreateCopy (alloc, dataString);
+      new->_version = version;
+      if (additionalInfoPtr)
+        {
+          new->_info = (const void*)&(new[1]);
+          memcpy ((void*)new->_info, additionalInfoPtr, additionalInfoSize);
+        }
+    }
+  
+  return new;
 }
 
 CFXMLNodeRef
@@ -51,23 +131,23 @@ CFXMLNodeCreateCopy (CFAllocatorRef alloc, CFXMLNodeRef origNode)
 const void *
 CFXMLNodeGetInfoPtr (CFXMLNodeRef node)
 {
-  return NULL;
+  return node->_info;
 }
 
 CFStringRef
 CFXMLNodeGetString (CFXMLNodeRef node)
 {
-  return NULL;
+  return node->_string;
 }
 
 CFXMLNodeTypeCode
 CFXMLNodeGetTypeCode (CFXMLNodeRef node)
 {
-  return 0;
+  return ((CFRuntimeBase*)node)->_flags.info;
 }
 
 CFIndex
 CFXMLNodeGetVersion (CFXMLNodeRef node)
 {
-  return 0;
+  return node->_version;
 }
