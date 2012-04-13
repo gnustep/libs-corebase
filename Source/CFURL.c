75a74214a3b89816b45a9cd495c0a45dfb4dce71
Stefan Bidigaray <stefanbidi@gmail.com>
2012-04-13 00:51:15 +0000

    Fix bug in last commit.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/corebase/trunk@35063 72102866-910b-0410-8b05-ffd578937521

--- Source/CFURL.c
+++ Source/CFURL.c
@@ -1648,14 +1648,14 @@ CFURLCreateStringByReplacingPercentEscapesUsingEncoding (CFAllocatorRef alloc,
 CFStringRef
 CFURLGetString (CFURLRef url)
 {
-  CF_OBJC_FUNCDISPATCH0(_kCFURLTypeID, CFStringRef, url, "relativeString")
+  CF_OBJC_FUNCDISPATCH0(_kCFURLTypeID, CFStringRef, url, "relativeString");
   return url->_urlString;
 }
 
 CFURLRef
 CFURLGetBaseURL (CFURLRef url)
 {
-  CF_OBJC_FUNCDISPATCH0(_kCFURLTypeID, CFStringRef, url, "baseURL")
+  CF_OBJC_FUNCDISPATCH0(_kCFURLTypeID, CFURLRef, url, "baseURL");
   return url->_baseURL;
 }
 
