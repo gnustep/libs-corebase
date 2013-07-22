86ff6b1ac69c5f6fe7bf25e563fb9160337fb10e
Stefan Bidigaray <stefanbidi@gmail.com>
2013-07-22 01:42:25 +0000

    * Source/GSCArray.h, * Source/GSCArray.c: Add function to sort and search C arrays.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/libs/corebase/trunk@36913 72102866-910b-0410-8b05-ffd578937521

---
+++ Source/GSCArray.c
@@ -0,0 +1,128 @@
+/* GSCArray.c
+   
+   Copyright (C) 2013 Free Software Foundation, Inc.
+   
+   Written by: Stefan Bidigaray
+   Date: July, 2013
+   
+   This file is part of GNUstep CoreBase library.
+   
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; see the file COPYING.LIB.
+   If not, see <http://www.gnu.org/licenses/> or write to the 
+   Free Software Foundation, 51 Franklin Street, Fifth Floor, 
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "GSCArray.h"
+
+#define GS_EXCHANGE_VALUES(_v1, _v2) do \
+{ \
+  const void *_tmp_; \
+  _tmp_ = _v1; \
+  _v1 = _v2; \
+  _v2 = _tmp_; \
+} while (0)
+
+void
+GSCArrayQuickSort (const void **array, CFIndex length,
+                   CFCOmpoaratorFunction comparator, void *context)
+{
+  if (length > 16) /* 16 is an arbritrary "small" number */
+    {
+      const void *value;
+      CFIndex pivot;
+      CFIndex stored;
+      CFIndex idx;
+      
+      pivot = length / 2;
+      value = array[pivot];
+      GS_EXCHANGE_VALUES (array[pivot], *end);
+      for (idx = 0, stored = 0 ; idx < length ; ++idx)
+        {
+          if ((*comp)(array[idx], value, context) == kCFCompareLessThan)
+            {
+              GS_EXHANGE_VALUES (array[idx], array[stored]);
+              ++stored;
+            }
+        }
+      GS_EXCHANGE_VALUES(*stored, *end);
+      
+      GSSortQuickSort (array, tmp - 1, comparator, context);
+      GSSortQuickSort (array + tmp + 1, length - tmp, comparator, context);
+    }
+  GSSortInsertionSort (array, length, comparator, context);
+}
+
+void
+GSCArrayInsertionSort (const void **array, CFIndex length,
+                       CFCOmpoaratorFunction comparator, void *context)
+{
+  CFIndex idx;
+  
+  for (idx = 1 ; idx < length ; ++idx)
+    {
+      int hole;
+      const void *value;
+      
+      hole = idx;
+      value = array[hole];
+      while (hole > 0 && value < array[hole - 1])
+        {
+          array[hole] = array[hole - 1];
+          --hole;
+        }
+      array[hole] = value;
+    }
+}
+
+CFIndex
+GSCArrayBSearch (const void **array, const void *key, CFIndex length,
+                 CFComparatorFunction comparator, void *context)
+{
+  CFIndex min;
+  CFIndex mid;
+  CFIndex max;
+  
+  min = 0;
+  max = length;
+  while (min < max)
+    {
+      CFComparisonResult r;
+      
+      mid = (min + max) / 2;
+      r = (*comparator) (key, array[mid], context);
+      if (r == kCFCompareLessThan)
+        {
+          max = mid - 1;
+          break;
+        }
+      else if (r == kCFCompareGreaterThan)
+        {
+          max = mid - 1;
+        }
+      else
+        {
+          min = mid + 1;
+        }
+    }
+  
+  return max + 1;
+}
+
+void
+GSCArrayHeapify (const void **array, CFIndex length,
+                 CFComparatorFunction comparator, void *context)
+{
+  
+}
---
+++ Source/GSCArray.h
@@ -0,0 +1,50 @@
+/* GSCArray.h
+   
+   Copyright (C) 2013 Free Software Foundation, Inc.
+   
+   Written by: Stefan Bidigaray
+   Date: July, 2013
+   
+   This file is part of GNUstep CoreBase library.
+   
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; see the file COPYING.LIB.
+   If not, see <http://www.gnu.org/licenses/> or write to the 
+   Free Software Foundation, 51 Franklin Street, Fifth Floor, 
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef __GSCARRAY_H__
+#define __GSCARRAY_H__
+
+#include "config.h"
+
+#include "CoreFoundation/CFBase.h"
+
+GS_PRIVATE void
+GSCArrayQuickSort (const void **array, CFIndex length,
+                   CFCOmpoaratorFunction comparator, void *context);
+
+GS_PRIVATE void
+GSCArrayInsertionSort (const void **array, CFIndex length,
+                       CFCOmpoaratorFunction comparator, void *context);
+
+GS_PRIVATE void
+GSCArrayHeapify (const void **array, CFIndex length,
+                 CFComparatorFunction comparator, void *context);
+
+GS_PRIVATE CFIndex
+GSCArrayBSearch (const void **array, const void *key, CFIndex length,
+                 CFComparatorFunction comparator, void *context);
+
+#endif /* __GSCARRAY_H__ */
